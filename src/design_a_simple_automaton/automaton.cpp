1cc3061eb9c1771a4bc3bb60c2e2829ab0bc30db
Boris Skert <boris.skert@gmail.com>
2022-07-10 10:32:30 +0200

    Setup kata

---
+++ src/design_a_simple_automaton/automaton.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+/**
+ * https://www.codewars.com/kata/5268acac0d3f019add000203/train/cpp
+ */
+class Automaton {
+public:
+    Automaton() {}
+
+    bool read_commands(const std::vector<char> &commands) {
+        return false;
+    }
+};
