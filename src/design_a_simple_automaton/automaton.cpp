a4d245550793e6025652c97fe6cb584927dc207d
Boris Skert <boris.skert@gmail.com>
2022-07-10 20:02:35 +0200

    Solve 'Design a Simple Automaton (Finite State Machine)' kata

--- src/design_a_simple_automaton/automaton.cpp
+++ src/design_a_simple_automaton/automaton.cpp
@@ -5,9 +5,64 @@
  */
 class Automaton {
 public:
-    Automaton() {}
+    Automaton();
 
-    bool read_commands(const std::vector<char> &commands) {
+    bool read_commands(const std::vector<char> &commands);
+};
+
+Automaton::Automaton() = default;
+
+class State {
+public:
+    virtual State *transition(char command) = 0;
+
+    virtual bool is_final() {
         return false;
     }
 };
+
+class Q1 : public State {
+    State *transition(char command) override;
+};
+
+class Q2 : public State {
+    State *transition(char command) override;
+
+    bool is_final() override {
+        return true;
+    }
+};
+
+class Q3 : public State {
+    State *transition(char command) override;
+};
+
+bool Automaton::read_commands(const std::vector<char> &commands) {
+    State *state = new Q1();
+
+    for (const char &command: commands) {
+        state = state->transition(command);
+    }
+
+    return state->is_final();
+}
+
+State *Q1::transition(char command) {
+    if (command == '1') {
+        return new Q2();
+    }
+
+    return this;
+}
+
+State *Q2::transition(char c) {
+    if (c == '0') {
+        return new Q3();
+    }
+
+    return this;
+}
+
+State *Q3::transition(char c) {
+    return new Q2();
+}
