dbc90e02a629a027ed709e1bbb86f532231574ad
Boris Skert <boris.skert@gmail.com>
2024-11-29 17:39:31 +0100

    Esolang Interpreters #2 - Custom Smallf*ck Interpreter (#11)
    * Prepare kata solution
    
    * WIP
    
    * Move 'Simple assembler interpreter's Machine to own namespace to avoid conflicts
    
    * Create first working solution
    
    * Clean up code

---
+++ include/custom_smallfuck_interpreter/interpreter.h
@@ -0,0 +1,8 @@
+#ifndef CPP_KATAS_INTERPRETER_H
+#define CPP_KATAS_INTERPRETER_H
+
+#include <string>
+
+std::string interpreter(const std::string &code, const std::string &tape);
+
+#endif //CPP_KATAS_INTERPRETER_H
